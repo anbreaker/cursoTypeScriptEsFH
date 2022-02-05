6f625b9f8149e8eb2651c40e6e0044ac2a41537d
anbreaker <antunez19@gmail.com>
2022-02-05 20:48:33 +0100

    init types ts

---
+++ 01-bases/types/any.js
@@ -0,0 +1,14 @@
+(function () {
+    var avenger = 123;
+    var exists;
+    var power;
+    avenger = 'Dr Strange';
+    // console.log(avenger.charAt(0));
+    console.log(avenger.charAt(0));
+    avenger = 150.232654;
+    // console.log(avenger.toFixed(2));
+    console.log(avenger.toFixed(2));
+    console.log(avenger.toFixed(1));
+    console.log(exists);
+    console.log(power);
+})();
